27d94352b83fafd17a680d286a14b20d423fb822
Egopr <93969256+Egopr@users.noreply.github.com>
2024-04-08 17:33:14 +0300

    pervay chastb

--- GUI_sum.py
+++ GUI_sum.py
@@ -13,6 +13,7 @@ def get_num():
     ui.num.selectAll()
     num = ui.num.selectedText()
     num = int(num)
+    ui.num.end(0)
     return num
 
 
@@ -20,36 +21,36 @@ def get_sum():
     ui.sum.selectAll()
     sum = ui.sum.selectedText()
     sum = int(sum)
+    ui.sum.end(0)
     return sum
 
-def get_befor():
-    ui.befor.selectAll()
-    befor = ui.befor.selectedText()
-    befor = int(befor)
-    return befor
+def get_before():
+    ui.before.selectAll()
+    before = ui.before.selectedText()
+    before = int(before)
+    ui.before.end(0)
+    return before
 
 
 def get_after():
     ui.after.selectAll()
     after = ui.after.selectedText()
     after = int(after)
+    ui.after.end(0)
     return after
 
 
 def write_list():
     ui.listWidget.clear()
     cur_row = ui.listWidget.currentRow()
-    mas = fs.random_summ(get_num(), get_sum(), get_befor(), get_after())
+    mas = fs.random_summ(get_num(), get_sum(), get_before(), get_after())
     str_mas = fs.str_mas(mas)
     for i in str_mas:
         ui.listWidget.insertItem(cur_row+1, i)
 
 
 ui.pushButton.clicked.connect(write_list)
-#ui.pushButton.clicked.connect(get_num)
-
-
-
+#ui.pushButton.clicked.connect(get_before)
 
 
 ui.show()
